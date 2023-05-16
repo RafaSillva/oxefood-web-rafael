3e4d0124e12c3b27b95baf7f1b96e33e5430b2dc
aluno@aluno <aluno@ifpe.com>
2023-05-16 15:08:59 -0300

    Modal

--- src/views/entregador/FormEntregador.jsx
+++ src/views/entregador/FormEntregador.jsx
@@ -3,7 +3,14 @@ import React from "react";
 import InputMask from 'react-input-mask';
 import { Link } from "react-router-dom";
 import { Button, Container, Divider, Form, Icon } from 'semantic-ui-react';
+import { ENDERECO_API } from "../util/constante";
 
+
+const ufList = [
+	{ key: 'o', text: 'Alagoas', value: 'AL' },
+	{ key: 'f', text: 'Paraíba', value: 'PB' },
+	{ key: 'm', text: 'Pernambuco', value: 'PE' },
+  ]
 
 class FormEntregador extends React.Component{
     
@@ -49,7 +56,7 @@ class FormEntregador extends React.Component{
             ativo: this.state.ativo
 		}
 	
-		axios.post("http://localhost:8082/api/entregador", EntregadorRequest)
+		axios.post(ENDERECO_API + "api/entregador", EntregadorRequest)
 		.then((response) => {
 			console.log('Entregador cadastrado com sucesso.')
 		})
@@ -261,6 +268,17 @@ class FormEntregador extends React.Component{
 										value={this.state.enferecoUf}
 			                           onChange={e => this.setState({enferecoUf: e.target.value})}
 									/>
+										
+								<Form.Select
+									fluid
+									label='UF'
+									options={ufList}
+									placeholder='Selecione'
+									value={this.state.enderecoEstado}
+									onChange={(e,{value}) => {
+										this.setState({enderecoEstado: value})
+									}}
+								/>
                                     
                                        <Form.Input
 										required
@@ -275,6 +293,27 @@ class FormEntregador extends React.Component{
                             
                                    </Form.Group>
                       
+								   <Form.Group inline>
+
+                               <label>Ativo: </label>
+
+                               <Form.Radio
+	                            label='Sim'
+	                            checked={this.state.ativo}
+	                            onChange={e => this.setState({
+		                        ativo: true
+	                                 })}
+                                  />
+
+                              <Form.Radio
+	                            label='Não'
+	                            checked={!this.state.ativo}
+	                            onChange={e => this.setState({
+		                        ativo: false
+	                                         })}
+                                               />
+ 
+                                     </Form.Group> 
 								
 							
 
